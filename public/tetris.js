41dbe80d64a2f63cc90bf32c7857264a9afb5fd3
Mateo.O <mateo.olivier72@gmail.com>
2026-02-20 16:46:56 +0100

    Debut de la logique tetris

--- public/tetris.js
+++ public/tetris.js
@@ -1,2 +1,9 @@
-lignes = 20
-cols = 10
+const LIGNES = 20;
+const COLONNES = 10;
+const TAILLE_BLOCS = 30;
+
+
+const canvas = document.getElementById("tetris");
+const context = canvas.getContext("2d");
+
+context.scale(TAILLE_BLOCS, TAILLE_BLOCS);
